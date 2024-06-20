07bef88df9c71870ca4120f76144f4d7519e2245
IceTheCoder <115871297+IceTheCoder@users.noreply.github.com>
2024-06-20 12:19:03 +0300

    center game over

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -4,7 +4,23 @@ export class Scene2 extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(20, 20, "Game over.");
+    const gameWidth = this.scale.width;
+    const gameHeight = this.scale.height;
+
+    // Use an auxilliary text box to get dimensions.
+    const aux = this.add.text(gameWidth / 2, gameHeight / 2, "Game over.");
+    const textBounds = aux.getBounds();
+
+    const textWidth = textBounds.width;
+    const textHeight = textBounds.height;
+
+    aux.destroy();
+
+    // Subtract half of the text width to center the text box.
+    let textX = gameWidth / 2 - textWidth / 2;
+    let textY = gameHeight / 2 - textHeight / 2;
+
+    const text = this.add.text(textX, textY, "Game over.");
   }
 }
 
