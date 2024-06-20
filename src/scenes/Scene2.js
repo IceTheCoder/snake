6f52b97fe508d09cb58d3a4a560a9c49fa4e7279
IceTheCoder <115871297+IceTheCoder@users.noreply.github.com>
2024-06-20 11:52:51 +0300

    Scene2 text

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -2,6 +2,10 @@ export class Scene2 extends Phaser.Scene {
   constructor() {
     super("gameOver");
   }
+
+  create() {
+    this.add.text(20, 20, "Game over.");
+  }
 }
 
 window.Scene2 = Scene2;
