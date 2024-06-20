1cdf6d3929b94620877d812f09d688bda310e6e4
IceTheCoder <115871297+IceTheCoder@users.noreply.github.com>
2024-06-20 14:13:08 +0300

    restart button

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -7,20 +7,17 @@ export class Scene2 extends Phaser.Scene {
     const gameWidth = this.scale.width;
     const gameHeight = this.scale.height;
 
-    // Use an auxilliary text box to get dimensions.
-    const aux = this.add.text(gameWidth / 2, gameHeight / 2, "Game over.");
-    const textBounds = aux.getBounds();
+    const text = this.add.text(gameWidth / 2, gameHeight / 2, "GAME OVER", { font: "48px Roboto" }).setOrigin(0.5, 0.5);
 
-    const textWidth = textBounds.width;
-    const textHeight = textBounds.height;
+    const restartButton = this.add.text(gameWidth / 2, (gameHeight / 2) + 75, "RESTART", {font: "36px Roboto"}).setOrigin(0.5, 0.5);
 
-    aux.destroy();
+    // https://snowbillr.github.io/blog/2018-07-03-buttons-in-phaser-3/
+    restartButton.setInteractive();
 
-    // Subtract half of the text width to center the text box.
-    let textX = gameWidth / 2 - textWidth / 2;
-    let textY = gameHeight / 2 - textHeight / 2;
-
-    const text = this.add.text(textX, textY, "Game over.");
+    restartButton.on('pointerover', () => { restartButton.setTint(0x7CFC00); });
+    restartButton.on('pointerout', () => { restartButton.setTint(0xffffff); });
+    restartButton.on('pointerdown', () => { restartButton.setTint(0x008000)})
+    restartButton.on('pointerup', () => { this.scene.start("playGame"); });
   }
 }
 
