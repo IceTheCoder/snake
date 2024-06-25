613341a8fb4fbdf305e35a0c7c5938749018450a
IceTheCoder <115871297+IceTheCoder@users.noreply.github.com>
2024-06-25 09:45:45 +0300

    Update Scene1.js

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -2,18 +2,35 @@ import Snake from '../scripts/Snake.js';
 import Fruit from '../scripts/Fruit.js';
 import Collision from '../scripts/Collision.js';
 
+/**
+ * Represents the main game scene.
+ * @class
+ * @extends Phaser.Scene
+ */
 export class Scene1 extends Phaser.Scene {
+  /**
+   * Constructs the Scene1 class.
+   */
   constructor() {
     super("playGame");
 
+    /** @type {number} */
     this.TILE_SIZE = 80;
+    /** @type {number} */
     this.GRID_WIDTH = 10;
-    this.GRID_HEIGHT = 10;  
+    /** @type {number} */
+    this.GRID_HEIGHT = 10;
+    /** @type {number} */
     this.timeBetweenEachMove = 200; // Adjust as necessary.
 
+    /** @type {Snake|null} */
     this.snake = null;
   }
 
+  /**
+   * Preload assets for the game.
+   * @method
+   */
   preload() {
     this.snake = new Snake(this, this.TILE_SIZE, this.GRID_WIDTH, this.GRID_HEIGHT, this.timeBetweenEachMove);
     this.snake.preload();
@@ -24,6 +41,10 @@ export class Scene1 extends Phaser.Scene {
     this.collision = new Collision(this, this.snake, this.fruit);
   }
 
+  /**
+   * Create game objects and initialize game settings.
+   * @method
+   */
   create() {
     this.snake.create();
 
@@ -35,17 +56,28 @@ export class Scene1 extends Phaser.Scene {
       left:Phaser.Input.Keyboard.KeyCodes.A,
       right:Phaser.Input.Keyboard.KeyCodes.D});
 
+    /** @type {Phaser.Input.Keyboard.CursorKeys} */
     this.cursors = this.input.keyboard.createCursorKeys(); // Define input keys
 
     this.fruit.create();
   }
 
+  /**
+   * Update the game state.
+   * @method
+   * @param {Phaser.Input.Keyboard.CursorKeys} cursors - Cursor keys for controlling the snake.
+   * @param {Object} wasd - WASD keys for controlling the snake.
+   */
   update() {
     this.snake.update(this.cursors, this.wasd);
 
     this.collision.update();
   }
 
+  /**
+   * Handle the game over state.
+   * @method
+   */
   gameOver() {
     this.scene.start("gameOver", { snakeLength: this.snake.snakeLength });
   }
