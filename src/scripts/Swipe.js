ed0ecd909c8f9f255b6fdf01e61688215b12dac9
IceTheCoder <115871297+IceTheCoder@users.noreply.github.com>
2024-06-25 10:38:11 +0300

    update Swipe.js

--- src/scripts/Swipe.js
+++ src/scripts/Swipe.js
@@ -8,6 +8,9 @@ const DIRECTION = Object.freeze({
   NONE: 'NONE',
 })
 
+/**
+ * Swipe detection.
+ */
 export class Swipe {
   // Private properties
   #scene;
@@ -16,36 +19,74 @@ export class Swipe {
   #lastPointerUpLocation;
   #swipeDirection;
 
+  /**
+   * Create a Swipe instance.
+   * @param {Phaser.Scene} scene - The phaser scene instance to listen for input events. 
+   * @param {Object} config - Configuration object for swipe detection.
+   */
   constructor(scene, config) {
+    /** @type {Phaser.Scene} */
     this.#scene = scene;
+
+    /** @type {Object} */
     this.#config = config;
+
+    /** @type {Phaser.Math.Vector2} */
     this.#lastPointerDownLocation = new Phaser.Math.Vector2(0, 0);
+
+    /** @type {Phaser.math.Vector2} */
     this.#lastPointerUpLocation = new Phaser.Math.Vector2(0, 0);
+    
+    /** @type {DIRECTION} */
     this.#swipeDirection = DIRECTION.NONE;
+
     this.#setupEvents();
   }
 
+  /**
+   * Sets up input events for swipe detection
+   * @private
+   */
   #setupEvents() {
     this.#scene.input.on(Phaser.Input.Events.POINTER_DOWN, this.#handlePointerDown, this);
     this.#scene.input.on(Phaser.Input.Events.POINTER_UP, this.#handlePointerUp, this);
+
+    // Cleanup events when scene shuts down
     this.#scene.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
       this.#scene.input.off(Phaser.Input.Events.POINTER_DOWN, this.#handlePointerDown, this);
       this.#scene.input.off(Phaser.Input.Events.POINTER_UP, this.#handlePointerUp, this);
     })
   }
 
+  /**
+   * Handles the pointer down event.
+   * @param {Phaser.Input.Pointer} pointer - The pointer object.
+   * @private
+   */
   #handlePointerDown(pointer) {
     this.#lastPointerDownLocation = pointer.position.clone();
   }
 
+  /**
+   * Handles the pointer up event.
+   * Triggers swipe detection and callback.
+   * @param {Phaser.Input.Pointer} pointer - The pointer object
+   * @private
+   */
   #handlePointerUp(pointer) {
     this.#lastPointerUpLocation = pointer.position.clone();
     this.#handleSwipe();
+    
+    // Trigger callback if swipe detected and callback is provided in config
     if (this.#swipeDirection !== DIRECTION.NONE && this.#config && this.#config.swipeDetectedCallback) {
       this.#config.swipeDetectedCallback(this.#swipeDirection);
     }
   }
 
+  /**
+   * Determined the direction of the swipe based on pointer positions.
+   * @private
+   */
   #handleSwipe() {
     if (this.#lastPointerDownLocation.x === this.#lastPointerUpLocation.x && this.#lastPointerDownLocation.y === this.#lastPointerUpLocation.y) {
       this.#swipeDirection = DIRECTION.NONE;
@@ -69,6 +110,5 @@ export class Swipe {
       return;
     }
     this.#swipeDirection = DIRECTION.DOWN;
-
   }
 }
