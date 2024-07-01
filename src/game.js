8b999b7f8804e26c85779216320573e49723b370
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-07-01 18:32:53 +0300

    header and footer

--- src/game.js
+++ src/game.js
@@ -13,7 +13,8 @@ const config = {
   width: 800,
   height: 800,
   backgroundColor: 0x000000,
-  scene: [Scene1]
+  scene: [Scene1],
+  parent: "phaser-game",
 }
 
 const button = document.getElementById("restart-button");
