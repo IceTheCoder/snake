136202bbece91d04b64dd0205727a62582e01768
IceTheCoder <cristian.gabriel131408@gmail.com>
2024-09-06 12:52:59 +0300

    fix score

--- src/scripts/Snake.js
+++ src/scripts/Snake.js
@@ -75,6 +75,8 @@ export default class Snake {
    * Initialize the snake, start moving the snake.
    */
   create() {
+    document.getElementById("score").innerHTML = `Score: ${this.snakeLength}`;
+
     if (localStorage.getItem("startingX") && localStorage.getItem("startingY")) {
       this.snakeX = localStorage.getItem("startingX") * this.TILE_SIZE + this.TILE_SIZE / 2;
       this.snakeY = localStorage.getItem("startingY") * this.TILE_SIZE + this.TILE_SIZE / 2;  
